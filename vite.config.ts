8651719547f32d6ffaecee8e14bb50850678524b
Samu-K <samu.kaarlela@gmail.com>
2025-11-29 11:02:59 +0200

    ios fixed

--- vite.config.ts
+++ vite.config.ts
@@ -22,19 +22,18 @@ export default defineConfig(async () => ({
 	clearScreen: false,
 	// 2. tauri expects a fixed port, fail if that port is not available
 	server: {
-		port: 1420,
-		strictPort: true,
-		host: host || false,
-		hmr: host
-			? {
-					protocol: "ws",
-					host,
-					port: 1421,
-				}
-			: undefined,
-		watch: {
-			// 3. tell Vite to ignore watching `src-tauri`
-			ignored: ["**/src-tauri/**"],
-		},
-	},
-}));
+	    host: host || false, // Listen on all addresses if TAURI_DEV_HOST is set, otherwise use localhost
+	    port: 1420, // Ensure this matches your tauri.conf.json
+	    strictPort: true,
+	    hmr: host
+	      ? {
+		  protocol: "ws",
+		  host,
+		  port: 1421,
+		}
+	      : undefined,
+	    watch: {
+	      // 3. tell vite to ignore watching `src-tauri`
+	      ignored: ["**/src-tauri/**"],
+	    },
+	  },}));
